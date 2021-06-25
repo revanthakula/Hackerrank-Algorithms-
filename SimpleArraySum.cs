c858c73f84581aedd72be5e0e525c0b9520714d0
revanthakula <86398882+revanthakula@users.noreply.github.com>
2021-06-25 16:11:40 +0530

    Update SimpleArraySum.cs

--- SimpleArraySum.cs
+++ SimpleArraySum.cs
@@ -14,18 +14,11 @@ using System;
 
 class Result
 {
-
-    /*
-     * Complete the 'simpleArraySum' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY ar as parameter.
-     */
-
     public static int simpleArraySum(List<int> ar)
-    {   int i,sum;
-        sum=0;
-        for(i=0;i< ar.Count; i++){
+    {   
+        int sum=0;
+        for(int i=0;i< ar.Count; i++)
+        {
             sum=sum+ar[i];
         }
         return sum;
