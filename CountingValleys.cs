8bf7aa37704a373a83c26c8b94b06f194f999e74
revanthakula <86398882+revanthakula@users.noreply.github.com>
2021-06-30 19:07:34 +0530

    Create CountingValleys.cs

---
+++ CountingValleys.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int countingValleys(int steps, string path)
+    {
+        var valleys=0;
+        var prev=0;
+        var tracker=0;
+        for(int i=0;i<steps;i++)
+        {
+            if(path[i]=='U') tracker++;
+            else tracker--;
+            if(prev<0&&tracker==0) valleys++;
+            prev=tracker;
+        }
+        return valleys;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
