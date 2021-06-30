2f4d5599e12bbef03ee4bf64a51f287333465900
revanthakula <86398882+revanthakula@users.noreply.github.com>
2021-06-30 19:13:04 +0530

    Create BillDivision.cs

---
+++ BillDivision.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static void bonAppetit(List<int> bill, int k, int b)
+    {
+        var sum=bill.Sum();
+        var anna=(sum-(bill[k]))/2;
+        if(b-anna==0)
+        {
+            System.Console.WriteLine("Bon Appetit");
+        }
+        else System.Console.WriteLine(b-anna);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+        int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.bonAppetit(bill, k, b);
+    }
+}
